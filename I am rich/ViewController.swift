5d40cab87db718cdb003658ff8e90695e56819b1
Avinash Magar <avinashmagar15@gmail.com>
2025-07-15 23:16:56 +0530

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Avinash Magar on 15/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
